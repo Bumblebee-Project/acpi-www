c86667cc3063fd008d3639d181e950d309888519
Lekensteyn <lekensteyn@gmail.com>
2011-09-22 23:22:50 +0200

    Added "validation" for acpidump uploads

--- includes/lib/AcpidumpUpload.php
+++ includes/lib/AcpidumpUpload.php
@@ -11,7 +11,25 @@ class AcpidumpUpload extends Upload {
     }
 
     public function validateUpload() {
-        
+        $dsdt_found = false;
+
+        // for efficiency, we're not validating the file contents which should
+        // be performed by acpixtract and iasl. Rather, we just check if the
+        // required DSDT line is found and leave that as is
+        $fp = $this->getFilePointer();
+
+        // a line should not be longer than 128 chars, in fact, a line is only
+        // 74 bytes (counting a LF)
+        while ($line = fread($fp, 128)) {
+            if (substr($line, 0, 4) == 'DSDT') {
+                $dsdt_found = true;
+                break;
+            }
+        }
+        fclose($fp);
+        if (!$dsdt_found) {
+            throw new UploadException("Table DSDT was not found");
+        }
     }
 }
 ?>
