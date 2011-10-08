a3e3cd1aee775ebac74e767c7e4b1ab5548e2b64
Lekensteyn <lekensteyn@gmail.com>
2011-10-08 16:51:27 +0200

    Database: add class for holding a mysqli connection

---
+++ includes/lib/Database.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @author Lekensteyn <lekensteyn@gmail.com
+ */
+
+/**
+ * Sorry for the ugliness ahead, I did not want to spend more time on just
+ * maintaining a simple DB connection ~Lekensteyn
+ */
+class Database extends mysqli {
+    private $instance;
+
+    public static function getInstance() {
+        if (!$this->instance) {
+            $this->instance = new self;
+        }
+        return $this->instance;
+    }
+
+    private function __construct() {
+        global $db_host, $db_user, $db_pass, $db_name;
+        parent::__construct($db_host, $db_user, $db_pass, $db_name);
+        if (!$this->connect_error) {
+            throw new DatabaseException(
+                'A database connection could not be established.'
+            );
+        }
+    }
+
+    public function prepare($query) {
+        $stmt = parent::prepare($query);
+        if (!$stmt) {
+            throw new DatabaseException(
+                'A database query could not be constructed.'
+            );
+        }
+        return $stmt;
+    }
+}
+class DatabaseException extends Exception {}
+?>
